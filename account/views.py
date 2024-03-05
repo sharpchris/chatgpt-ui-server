9f01332254cdd0dd31b7a1a7acaeacfa09a00c03
Chris Sharp <sharpchris@gmail.com>
2024-03-04 21:19:24 -0500

    read list of permitted emails from a csv

--- account/views.py
+++ account/views.py
@@ -1,3 +1,5 @@
+import csv
+import os
 from rest_framework.response import Response
 from rest_framework import status
 from dj_rest_auth.registration.views import RegisterView
@@ -19,7 +21,7 @@ class RegistrationView(RegisterView):
         serializer.is_valid(raise_exception=True)
 
         # Check to see if the user is in the allow list
-        valid_emails = ["student8@ufl.edu","student9@ufl.edu","student10@ufl.edu"]
+        valid_emails = get_emails_from_csv()
         if request.data['email'] not in valid_emails:
             print(f"The email address {request.data['email']} was not in the list of valid addresses")
             response = Response({'detail': f"The email address {request.data['email']} is not in the list of allowed emails. Please use your UF email account, or contact the instructor."}, status=status.HTTP_403_FORBIDDEN)
@@ -40,3 +42,32 @@ class RegistrationView(RegisterView):
                 response = Response(status=status.HTTP_204_NO_CONTENT, headers=headers)
 
         return response
+
+def get_emails_from_csv():
+    """
+    Extracts a list of emails from the specified CSV file.
+
+    Args:
+    file_path (str, optional): Path to the CSV file. Defaults to "valid_emails.csv".
+
+    Returns:
+    list: A list of extracted email addresses.
+    """
+
+    # Get the script's directory path
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Combine the script directory and filename to form the complete path
+    file_path = os.path.join(script_dir, "jury_accounts/valid_emails.csv")
+
+    print(f"Checking the file path: {file_path}", flush=True)
+
+    emails = []
+    with open(file_path, "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            email = row[0] # Assuming emails are in the first column
+            emails.append(email)
+    
+    print(f"I found {len(emails)} emails")
+    return emails
